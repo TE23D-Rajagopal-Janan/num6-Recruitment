b09fcba0a8b4747670ee168b1602e90075b5b1a0
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-10-14 09:23:03 +0200

    hjk

---
+++ Recruitment/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> names = ["Janan", "Adam", "Imad"];
+
+string name = Console.ReadLine();
+//names[1] = "Janne";
+names.Add("Kerem");
+//names.Remove("Kerem");
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+if (names.Contains("Janan") == true); 
+{
+    Console.WriteLine("Yes Janan är med i listan");
+}
+
+Console.ReadLine();
